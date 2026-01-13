027894e0bf323d0f5a9e058fb58803c55c3bf3f9
Rohan Rai <rairohannit23@gmail.com>
2026-01-14 00:22:37 +0530

    added basic layout and architecture

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,11 +1,35 @@
 import { Tabs } from "expo-router"
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
 
 const TabLayout = () =>{
     return (
-        <Tabs>
-            <Tabs.Screen name="index" options={{title: 'Shop'}}/>
+        <SafeAreaView style={styles.safeArea}>
+            <Tabs 
+            screenOptions={{
+                tabBarActiveTintColor: '#1bc464',
+                tabBarInactiveTintColor: 'grey',
+                tabBarLabelStyle: { fontSize: 16},
+                tabBarStyle: {
+                    borderTopLeftRadius: 20,
+                    borderTopRightRadius: 20,
+                    paddingTop: 10,
+                },
+                headerShown: false,
+            }}>
+            <Tabs.Screen name="index" options={{
+                headerShown: false,
+            }}/>
             <Tabs.Screen name="orders" options={{title: 'Orders'}}/>
         </Tabs>
+        </SafeAreaView>
+        
     )
 }
 export default TabLayout;
+
+const styles = StyleSheet.create({
+    safeArea:{
+        flex: 1,
+    }
+});
